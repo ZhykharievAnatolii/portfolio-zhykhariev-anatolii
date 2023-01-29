b38bc783f20fd9733c0df114e47ed557de74eb79
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-29 08:41:14 +0200

    add footer and main js

--- portfolio/src/Application.js
+++ portfolio/src/Application.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Header} from "./Header";
+import {Main} from "./Main";
+import {Footer} from "./Footer";
 
 export const Application=()=>
     <div className='Wrapper'>
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {createRoot} from "react-dom/client";
 import {Application} from "./Application";
+
+import 'bootstrap';
 import {Header} from "./Header";
 import './index.css'
 const root=createRoot(document.querySelector('#root'));
